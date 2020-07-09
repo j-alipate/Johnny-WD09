a6fcdc3fac9569c40549220f09a94ecbbe1a3b6e
Johnny <4018196+j-alipate@users.noreply.github.com>
2020-07-09 17:27:18 +1200

    gatsby tutorials

---
+++ tutorial-part-four/gatsby-config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Title from siteMetadata'
+  },
+  plugins: [
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+  ],
+}
